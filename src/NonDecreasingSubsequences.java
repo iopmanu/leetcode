f60edee6c2e7afe28608aac1368627b3af445151
Ivan Doronin <i.doronin@vkteam.ru>
2026-03-28 18:41:08 +0300

    NonDecreasingSubsequences via backtracking

---
+++ src/NonDecreasingSubsequences.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author i.doronin
+ * Given an integer array nums, return all the different possible non-decreasing subsequences of the given array
+ * with at least two elements. You may return the answer in any order.
+ */
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> retval = new HashSet<>();
+        List<Integer> subsequence = new ArrayList<>();
+        traverse(retval, subsequence, nums, 0);
+
+        return new ArrayList<>(retval);
+    }
+
+    public void traverse(Set<List<Integer>> retval, List<Integer> subsequence, int[] nums, int index) {
+        if (subsequence.size() >= 2) {
+            retval.add(new ArrayList<>(subsequence));
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            if (subsequence.isEmpty() || subsequence.getLast() <= nums[i]) {
+                subsequence.add(nums[i]);
+                traverse(retval, subsequence, nums, i + 1);
+                subsequence.removeLast();
+            }
+        }
+    }
+}
